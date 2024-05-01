6ec6c8eefd36f13f94aabe9b7633fab7c63bad29
T0nete <toni.barrantes.montilla64@gmail.com>
2024-05-01 18:28:28 +0200

    Add propt and context to  openai chat

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,7 @@
 import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { Message, OpenAIStream, StreamingTextResponse } from 'ai';
+import { getContext } from '@/lib/pinecone/pinecone-context';
+import { NextResponse } from 'next/server';
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
@@ -11,15 +13,48 @@ export const dynamic = 'force-dynamic';
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  const lastMessage = messages[messages.length - 1];
+  const context = await getContext(
+    lastMessage.content,
+    'TFM_Grupo4_Entrega_Final (1).pdf'
+  );
+
+  console.log(context);
+
+  const prompt = [
+    {
+      role: 'system',
+      content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
+    The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+    AI is a well-behaved and well-mannered individual.
+    AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
+    AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+    AI assistant is a big fan of Pinecone and Vercel.
+    START CONTEXT BLOCK
+    ${context}
+    END OF CONTEXT BLOCK
+    AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
+    If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
+    AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
+    AI assistant will not invent anything that is not drawn directly from the context.
+    `,
+    },
+  ];
+
+  const userMessages = messages.filter(
+    (message: Message) => message.role === 'user'
+  );
+
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     stream: true,
-    messages,
+    messages: [...prompt, ...userMessages],
   });
 
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
+
   // Respond with the stream
   return new StreamingTextResponse(stream);
 }
