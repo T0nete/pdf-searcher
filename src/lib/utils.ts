0ba1defe105ad42481f8d53f88977e79cadf1435
T0nete <toni.barrantes.montilla64@gmail.com>
2024-05-01 18:25:05 +0200

    Add convertToAscii function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,9 @@ export const createResponseObject = (
     errorMessage,
   };
 };
+
+export const convertToAscii = (inputString: string) => {
+  // remove non ascii characters
+  const asciiString = inputString.replace(/[^\x00-\x7F]+/g, '');
+  return asciiString;
+};
