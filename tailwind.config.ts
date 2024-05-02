2141c506a723e2f2bdb451ca2399715c86441ff9
T0nete <toni.barrantes.montilla64@gmail.com>
2024-05-02 23:12:58 +0200

    Update tailwind.config.ts with new font and color settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        dosis: ['Dosis', 'sans-serif'],
+      },
+      colors: {
+        'brand-orange': '#FF4F18',
+        dark: '#2c2c2c',
+        // dark: '#141517',
+        // 'light-gray': '#F2F4F7',
+        'light-gray': '#3e3e3e',
+        'dark-gray': '#333333',
       },
     },
   },
