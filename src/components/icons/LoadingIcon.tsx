4358aeb3842b1acd7055709ca43ab9c1af09e141
T0nete <toni.barrantes.montilla64@gmail.com>
2024-05-03 22:51:48 +0200

    Fix loading svg property

--- src/components/icons/LoadingIcon.tsx
+++ src/components/icons/LoadingIcon.tsx
@@ -18,7 +18,7 @@ const LoadingIcon = (props: Props) => {
         cy="12"
         r="10"
         stroke="currentColor"
-        stroke-width="4"
+        strokeWidth="4"
       ></circle>
       <path
         className="opacity-75"
