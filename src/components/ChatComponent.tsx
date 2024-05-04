8ceca0676565287dc24ad34082bcf8ddfcbc0a23
T0nete <toni.barrantes.montilla64@gmail.com>
2024-05-04 22:09:10 +0200

    refactor: Improve chat component UI and functionality

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -6,17 +6,31 @@ const ChatComponent = () => {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map((m) => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))}
+    <div className="flex flex-col w-full max-w-md mx-auto stretch justify-between">
+      <div className="overflow-y-auto max-h-[85vh]">
+        {messages.map((m) => (
+          <div
+            key={m.id}
+            className={`flex whitespace-pre-wrap mb-2 ${
+              m.role === 'user' ? 'justify-end' : 'justify-start'
+            }`}
+          >
+            <p
+              className={`rounded-md py-2 px-4 ${
+                m.role === 'user'
+                  ? 'justify-end bg-brand-orange text-white'
+                  : 'text-start bg-light-gray'
+              }`}
+            >
+              {m.content}
+            </p>
+          </div>
+        ))}
+      </div>
 
       <form onSubmit={handleSubmit}>
         <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          className="w-full p-2 bg-dark-gray border border-light-gray rounded shadow-xl focus:border-brand-orange focus:outline-none"
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
