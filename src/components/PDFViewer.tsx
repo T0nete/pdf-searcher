40a0c1fd003778bb48c425b4a7cc7618f4f4b795
T0nete <toni.barrantes.montilla64@gmail.com>
2024-05-06 22:30:00 +0200

    fix data fetching from a client component

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -1,12 +1,30 @@
+'use client';
+
+import React from 'react';
 import { getPdfUrl } from '@/lib/supabase/supabase-chats';
+import LoadingIcon from './icons/LoadingIcon';
 
 type PDFViewerProps = {
   chatId: string;
 };
 
-const PDFViewer = async (props: PDFViewerProps) => {
-  const pdfUrl = await getPdfUrl(props.chatId);
+const PDFViewer = (props: PDFViewerProps) => {
+  const [pdfUrl, setPdfUrl] = React.useState<string | null>(null);
+  const [isLoading, setIsLoading] = React.useState(true);
 
+  React.useEffect(() => {
+    const fetchPdfUrl = async () => {
+      const url = await getPdfUrl(props.chatId);
+      setPdfUrl(url);
+      setIsLoading(false);
+    };
+
+    fetchPdfUrl();
+  }, [props.chatId]);
+
+  if (isLoading) {
+    return <LoadingIcon className="h-12 w-12 text-brand-orange" />;
+  }
   if (!pdfUrl) {
     return <div>PDF not found</div>;
   }
