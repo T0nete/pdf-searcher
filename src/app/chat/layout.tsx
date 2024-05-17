381e1018bf0c2d2c4f30e2a23111586497b20743
T0nete <toni.barrantes.montilla64@gmail.com>
2024-05-18 00:42:55 +0200

    chore: Update layout styles for ChatLayout and Header components

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -6,7 +6,7 @@ export default function ChatLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col h-full ">
+    <div className="flex flex-col h-full gap-2">
       <Header />
       <main className="flex-1 overflow-auto px-4 md:px-0">{children}</main>
     </div>
