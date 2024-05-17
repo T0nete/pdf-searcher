3f8126947c03bd49cb6ce53ccb82a2450015ca04
T0nete <toni.barrantes.montilla64@gmail.com>
2024-05-17 22:41:26 +0200

    fix height

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -6,7 +6,7 @@ export default function ChatLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-dvh">
       <Header />
       <main className="flex-1 overflow-auto">{children}</main>
     </div>
