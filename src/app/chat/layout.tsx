e2d7393f993bd0d6d3eab21e66905c8dd68273c0
T0nete <toni.barrantes.montilla64@gmail.com>
2024-05-18 00:38:15 +0200

    fix styles

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -6,11 +6,9 @@ export default function ChatLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col justify-center h-full">
-      <div className="px-4">
-        <Header />
-        <main className="flex-1 overflow-auto">{children}</main>
-      </div>
+    <div className="flex flex-col h-full px-4 md:px-0">
+      <Header />
+      <main className="flex-1 overflow-auto">{children}</main>
     </div>
   );
 }
