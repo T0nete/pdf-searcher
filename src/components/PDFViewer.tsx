7084d7a39476a7ccbe89eb6f0df91c985e39df8b
T0nete <toni.barrantes.montilla64@gmail.com>
2024-05-18 18:23:52 +0200

    Update iframe to use google doc

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -11,7 +11,7 @@ const PDFViewer = (props: PDFViewerProps) => {
 
   return (
     <iframe
-      src={props.pdfUrl}
+      src={`https://docs.google.com/gview?url=${props.pdfUrl}&embedded=true`}
       width="100%"
       height="100%"
       className="rounded-lg shadow-xl"
