128eac694a3226187a71c1624919fb80b68a9772
T0nete <toni.barrantes.montilla64@gmail.com>
2024-06-23 16:57:43 +0200

    feat: change pdf viewer library

--- src/components/ChatMainContent.tsx
+++ src/components/ChatMainContent.tsx
@@ -7,7 +7,6 @@ import { Chat } from '@/types/supabase-databse';
 import EyeHide from '@/components/icons/EyeHide';
 import EyeShow from '@/components/icons/EyeShow';
 import useMediaQuery from '@/hooks/useMediaQuery';
-import PDFViewerServer from './PDFViewerContainer';
 
 type Props = {
   chatData: Chat;
@@ -15,7 +14,7 @@ type Props = {
 
 const ChatMainContent = (props: Props) => {
   const { targetReached } = useMediaQuery(768);
-  const [showPDF, setShowPDF] = React.useState(!targetReached);
+  const [showPDF, setShowPDF] = React.useState(false);
 
   const handleShowPDF = () => {
     setShowPDF((prev) => !prev);
@@ -30,9 +29,8 @@ const ChatMainContent = (props: Props) => {
   return (
     <div className="relative flex flex-col md:flex-row h-full pb-6 gap-6">
       <div
-        className={`absolute bottom-6 right-0 md:hidden ${
-          showPDF ? 'block' : 'hidden'
-        }`}
+        className={`absolute bottom-6 right-0 md:hidden ${showPDF ? 'block' : 'hidden'
+          }`}
       >
         <button
           className="bg-brand-orange p-2 rounded-full hover:bg-brand-orange-hover duration-200 transition-colors"
@@ -42,12 +40,11 @@ const ChatMainContent = (props: Props) => {
         </button>
       </div>
       <div className={`w-full h-full ${showPDF ? 'block' : 'hidden'}`}>
-        <PDFViewerServer pdfUrl={props.chatData?.pdf_url ?? ''} />
+        <PDFViewer pdfUrl={props.chatData?.pdf_url ?? ''} />
       </div>
       <div
-        className={`h-full w-full ${
-          !showPDF ? 'md:w-3/4 md:max-w-3xl' : 'hidden md:block md:max-w-md'
-        } mx-auto`}
+        className={`h-full w-full ${!showPDF ? 'md:w-3/4 md:max-w-3xl' : 'hidden md:block md:max-w-md'
+          } mx-auto`}
       >
         <ChatComponent
           chatId={props.chatData.id}
--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -1,26 +1,52 @@
+// type PDFViewerProps = {
+//   pdfUrl: string;
+// };
+
+// const PDFViewer = ({ pdfUrl }: PDFViewerProps) => {
+//   if (!pdfUrl) {
+//     return <div>PDF not found</div>;
+//   }
+
+//   return (
+//     <iframe
+//       key={pdfUrl}
+//       src={`https://docs.google.com/gview?url=${pdfUrl}&embedded=true`}
+//       width="100%"
+//       height="100%"
+//       className="rounded-lg shadow-xl"
+//       onError={(e) => { console.log('IFRAME', e) }}
+//     />
+//   );
+// };
+
+// export default PDFViewer;
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 type PDFViewerProps = {
   pdfUrl: string;
 };
 
 const PDFViewer = ({ pdfUrl }: PDFViewerProps) => {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
   if (!pdfUrl) {
     return <div>PDF not found</div>;
   }
 
   return (
-    <iframe
-      key={pdfUrl}
-      // src={`https://drive.google.com/file/d/${pdfUrl}}/preview`}
-      src={`https://docs.google.com/gview?url=${pdfUrl}&embedded=true`}
-      width="100%"
-      height="100%"
-      className="rounded-lg shadow-xl"
-    />
+    <div className="rounded-lg shadow-xl h-100 w-100">
+      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.0.279/build/pdf.worker.min.js`}>
+        <Viewer fileUrl={pdfUrl} plugins={[defaultLayoutPluginInstance]} />
+      </Worker>
+    </div>
   );
 };
 
 export default PDFViewer;
+
